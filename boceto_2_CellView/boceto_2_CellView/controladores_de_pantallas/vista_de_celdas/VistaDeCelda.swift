4ef9080cd9ee6f44a26d7f9df44d789fb22e47db
alumno <alumno@desktop-1hcfdjv.uacj.mx>
2024-10-07 10:26:33 -0600

    Programa para JSON

---
+++ boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/vista_de_celdas/VistaDeCelda.swift
@@ -0,0 +1,13 @@
+//
+//  VistaDeCelda.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 10/7/24.
+//
+
+import UIKit
+
+class VistaDeCelda: UICollectionViewCell {
+    @IBOutlet weak var etiqueta: UILabel!
+    
+}
