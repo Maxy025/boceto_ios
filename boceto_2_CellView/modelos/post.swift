5c61c4906b478a51f3cb4024b9d4031131690137
alumno <alumno@desktop-1hcfdjv.uacj.mx>
2024-10-09 10:42:38 -0600

    Actualizacion de la APIRest

---
+++ boceto_2_CellView/modelos/post.swift
@@ -0,0 +1,16 @@
+//
+//  post.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 10/9/24.
+//
+
+import Foundation
+
+
+struct Publicacion : Codable{
+    var id:Int
+    var userId:Int
+    var title:String
+    var body:String
+}
